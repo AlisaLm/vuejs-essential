4bc098e57cae0e9371bc86ac8f49c3bcfa8f3f67
AlisaLm <1121044949@qq.com>
2021-01-03 14:20:42 +0800

    thieheader

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,29 @@
   <div class="container">
   <div class="jumbotron">
   <div class="container">
+    <h1>Welcome form Bootstrap</h1>
+    <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    <p><a class="btn btn-primary btn-lg" href="http://www.bootcss.com/" role="button" target="_blank"> Learn more <span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span>
+    </a></p>
+  </div>
+  </div>
+
+  <div class="jumbotron">
+  <div class="container">
   <h1>Welcome form Bootstrap</h1>
   <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <p><a class="btn btn-primary btn-lg" href="http://www.bootcss.com/" role="button" target="_blank"> Learn more <span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span>
-  </a></p>
+  <p>
+  <i class="fa fa-camera"></i>
+  <i class="fa fa-camera fa-2x"></i>
+  <i class="fa fa-camera fa-3x"></i>
+  <i class="fa fa-camera fa-4x"></i>
+  <i class="fa fa-camera fa-5x"></i>
+  <i class="fa fa-camera fa-spin"></i>
+  </p>
   </div>
   </div>
   </div>
+
 </template>
 
 <script>
@@ -21,7 +37,9 @@ export default {
 
 <style lang="scss">
 $icon-font-path: "~bootstrap-sass/assets/fonts/bootstrap/";
+$fa-font-path: "~font-awesome/fonts/";
 @import "~bootstrap-sass/assets/stylesheets/_bootstrap";
+@import "~font-awesome/scss/font-awesome";
 
 body > .container { margin-top: 15px;}
 </style>
