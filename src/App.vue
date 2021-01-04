fc4c2071d51a0f94cfba906985972da851e252ab
AlisaLm <1121044949@qq.com>
2021-01-04 15:18:44 +0800

    导航栏切换

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,37 @@
-<template lang="pug">
-  <div class="container">
-  <div class="jumbotron">
-  <div class="container">
-    <h1>Welcome form Bootstrap</h1>
-    <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-    <p><a class="btn btn-primary btn-lg" href="http://www.bootcss.com/" role="button" target="_blank"> Learn more <span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span>
-    </a></p>
+<template>
+  <div id="wrap">
+    <TheHeader/>
   </div>
-  </div>
-
-  <div class="jumbotron">
-  <div class="container">
-  <h1>Welcome form Bootstrap</h1>
-  <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <p>
-  <i class="fa fa-camera"></i>
-  <i class="fa fa-camera fa-2x"></i>
-  <i class="fa fa-camera fa-3x"></i>
-  <i class="fa fa-camera fa-4x"></i>
-  <i class="fa fa-camera fa-5x"></i>
-  <i class="fa fa-camera fa-spin"></i>
-  </p>
-  </div>
-  </div>
-  </div>
-
 </template>
 
 <script>
+import TheHeader from "@/components/layouts/TheHeader";
 
 export default {
   name: 'App',
+  components: {
+    TheHeader
+  }
 
 }
 </script>
 
 <style lang="scss">
+$container-large-desktop: 1200px;
+$btn-primary-bg: #00b5ad;
+$btn-primary-border: #00b5ad;
+$label-primary-bg: #00b5ad;
+$pagination-active-bg: #00b5ad;
+$pagination-active-border: #00b5ad;
+$pagination-color: #00b5ad;
+$input-border-focus: #00b5ad;
+$link-color: #12c4c5;
+$link-hover-color: #22ddde;
 $icon-font-path: "~bootstrap-sass/assets/fonts/bootstrap/";
 $fa-font-path: "~font-awesome/fonts/";
+
 @import "~bootstrap-sass/assets/stylesheets/_bootstrap";
 @import "~font-awesome/scss/font-awesome";
-
-body > .container { margin-top: 15px;}
+@import "./styles/main";
+@import "./styles/extra";
 </style>
